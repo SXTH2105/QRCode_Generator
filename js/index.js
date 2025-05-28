80e97e65809722179bae7e1c466059463f14c35c
Seth <sokpisethkong68@gmail.com>
2025-05-28 21:57:07 +0700

    add download button

--- js/index.js
+++ js/index.js
@@ -11,5 +11,25 @@ function generatorQRCode() {
     const apiURL = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${encodeURIComponent(text)}`;
 
     qrImage.src = apiURL;
+    
+    qrImage.onload = function() {
+        downloadBtn.style.display = "inline"
+    };
+
+    downloadBtn.onclick = function () {
+        fetch(apiURL)
+            .then(response => response.blob())
+            .then(blob => {
+                const blobURL = URL.createObjectURL(blob);
+                const link = document.createElement("a");
+                link.href = blobURL;
+                link.download = "qrcode.png";
+                document.body.appendChild(link);
+                link.click();
+                document.body.removeChild(link);
+                URL.revokeObjectURL(blobURL);
+            });
+    };
+
 
 }
